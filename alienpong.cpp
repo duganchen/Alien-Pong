d8380de5e52c06543741f6b0c1fcb7781e28ead6
Dugan Chen <thedoogster@gmail.com>
2014-06-21 09:42:34 -0700

    CMake build system started

--- alienpong.cpp
+++ alienpong.cpp
@@ -1,10 +1,17 @@
+/*
 #include <QtGui/QApplication>
 #include "gui.h"
+*/
+
 
 int main(int argc, char *argv[])
 {
+	/*
     QApplication a(argc, argv);
     GUI w;
     w.show();
     return a.exec();
+	*/
+
+	return 0;
 }
